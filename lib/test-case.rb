1c5c869df22453b383df6ec52297644a5f65e9be
Scott H Lee <slee@logicfirstllc.com>
2020-07-16 14:59:25 +0000

    Automatically backed up by Learn

--- lib/test-case.rb
+++ lib/test-case.rb
@@ -1,6 +1,7 @@
 magic_exit_number = 7
 count = 0
-while count < 10 do
+while count < 10 && 
+count != do
   break if count ==
   magic_exit_number
   puts "I am the nnumber #{count}, I love to count!"
